5e2c4a52d122e237a4d3e36523cb64f6b425be28
Raman Valasevich <volosevich.roma@gmail.com>
2021-11-18 11:43:49 +0300

    Add dot-env

--- src/api/serverConfig.js
+++ src/api/serverConfig.js
@@ -1,4 +1,4 @@
-export const baseURL = process.env.SERVER_URL ?? 'http://localhost:5000/api';
+export const baseURL = process.env.API_URL ?? 'http://localhost:5000/api';
 console.log(baseURL);
 export const serverConfig = {
   registrationURL: `${baseURL}/registration`,
